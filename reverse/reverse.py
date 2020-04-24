2282de998791ae6ce8f8bb5f4d9697e81a1bddbd
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-24 09:18:41 -0700

    completes reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -45,6 +45,11 @@ class LinkedList:
         # if we've gotten here, then the target node isn't in our list
         return False
 
-    def reverse_list(self, node, prev):
+    def reverse_list(self, node, prev=None):
         # You must use recursion for this solution
-        pass
+        if not node:
+            return
+        next_node = node.next_node
+        node.next_node = prev
+        self.head = node
+        self.reverse_list(next_node, node)
